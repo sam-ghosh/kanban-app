f762e0bf6eb0702e45f90a4e7802b240df309e35
Som Ghosh <som.ghosh@gmail.com>
2016-06-06 13:00:42 +0100

    working copy

---
+++ app/components/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Notes from './notes';
+import uuid from 'uuid';
+
+const notes = [
+    {
+        id: uuid.v4(),
+        task: 'Learn React'
+    }, {
+        id: uuid.v4(),
+        task: 'Do laundry'
+    }
+];
+
+export default () => (
+    <div>
+        <button onClick={() => console.log('add note')}>+</button>
+        <Notes notes={notes} />
+    </div>
+
+);
+//
+// export default class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             notes : [
+//                 {
+//                     id: uuid.v4(),
+//                     task: 'Learn React'
+//                 }, {
+//                     id: uuid.v4(),
+//                     task: 'Do laundry'
+//                 }
+//             ]
+//         }
+//     }
+//
+//     render() {
+//         const {notes} = this.notes;
+//
+//         return (
+//             <div>
+//                 <button onClick={() => console.log('add note')}>+</button>
+//                 <Notes notes={notes} />
+//
+//             </div>
+//         );
+//     }
+// }
---
+++ app/components/notes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+
+export default ({notes}) => (
+
+  <ul>{notes.map(note =>
+    <li key={note.id}>{note.task}</li>
+  )}</ul>
+);
