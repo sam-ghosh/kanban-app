650a91d4be6f754dfceb066714b84bb660ee6854
Som Ghosh <som.ghosh@gmail.com>
2016-06-06 13:06:14 +0100

    broken code

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Notes from './notes';
 import uuid from 'uuid';
 
-const notes = [
+/*const notes = [
     {
         id: uuid.v4(),
         task: 'Learn React'
@@ -18,34 +18,34 @@ export default () => (
         <Notes notes={notes} />
     </div>
 
-);
-//
-// export default class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//
-//         this.state = {
-//             notes : [
-//                 {
-//                     id: uuid.v4(),
-//                     task: 'Learn React'
-//                 }, {
-//                     id: uuid.v4(),
-//                     task: 'Do laundry'
-//                 }
-//             ]
-//         }
-//     }
-//
-//     render() {
-//         const {notes} = this.notes;
-//
-//         return (
-//             <div>
-//                 <button onClick={() => console.log('add note')}>+</button>
-//                 <Notes notes={notes} />
-//
-//             </div>
-//         );
-//     }
-// }
+);*/
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes : [
+                {
+                    id: uuid.v4(),
+                    task: 'Learn React'
+                }, {
+                    id: uuid.v4(),
+                    task: 'Do laundry'
+                }
+            ]
+        }
+    }
+
+    render() {
+        const {notes} = this.notes;
+
+        return (
+            <div>
+                <button onClick={() => console.log('add note')}>+</button>
+                <Notes notes={notes} />
+
+            </div>
+        );
+    }
+}
