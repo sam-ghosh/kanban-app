d73e0856b86b1202e59b9e1719f2cb03cccd4891
Som Ghosh <som.ghosh@gmail.com>
2016-06-07 01:20:03 +0100

    add and delete notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -20,32 +20,60 @@ export default () => (
 
 );*/
 
+function fn () {
+    console.log('xxx');
+}
+
 export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            notes : [
-                {
-                    id: uuid.v4(),
-                    task: 'Learn React'
-                }, {
-                    id: uuid.v4(),
-                    task: 'Do laundry'
-                }
-            ]
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
         }
+      ]
     }
+  }
+  render = () => {
+    const {notes} = this.state;
+64
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <button onClick={this.removeLastNote}>-</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
 
-    render() {
-        const {notes} = this.notes;
+  addNote = () => {
+      this.setState({
+          notes: this.state.notes.concat([
+              {
+                  id: uuid.v4(),
+                  task: 'New Task'
+              }
+          ])
+      });
+      console.log('task added');
+  }
 
-        return (
-            <div>
-                <button onClick={() => console.log('add note')}>+</button>
-                <Notes notes={notes} />
+  removeLastNote = () => {
 
-            </div>
-        );
-    }
+      const x = this.state.notes.pop();
+      console.log(`deleted item = ${x}`);
+
+      this.setState({
+          notes: this.state.notes
+      });
+      console.log('task removed');
+  }
+
 }
