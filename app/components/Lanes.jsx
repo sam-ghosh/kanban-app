ecee1cb965a1661bf27ed8c79fcef93c889f8c3a
Som Ghosh <som.ghosh@gmail.com>
2016-06-14 12:20:36 +0100

    trying to create error

--- app/components/Lanes.jsx
+++ app/components/Lanes.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Lane from './Lane';
 
-export default({lanes}) => (
+const Lanes = ({lanes}) => (
     <div className="lanes">
         {lanes.map(lane => (
             <Lane className="lane" key={lane.id} lane={lane}/>
         ))}
     </div>
-)
+);
+
+Lanes.propTypes = {
+    lanes: React.PropTypes.array.isRequired
+};
+
+Lanes.defaultProps = {
+    lanes: []
+};
+
+export default Lanes;
 
 
